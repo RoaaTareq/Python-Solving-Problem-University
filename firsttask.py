8af8122533d18a4ef42b8a44da1299d9ed508b4c
Roaaabushaqrah <roaa.abushaqrah@gmail.com>
2024-03-01 22:11:52 +0300

    task 1

---
+++ firsttask.py
@@ -0,0 +1,21 @@
+"""
+Check if a number is odd or even. 
+Take the number 5 for example. 
+If you want to check, 
+you must divide it by 2. 
+If the result is 0, it's even; otherwise, it's odd
+"""
+x=5,
+if 5 /2 == 0 :
+    print('even')
+else:
+    print('odd')
+
+
+"""
+program sum all num on list
+
+"""
+mylist=[1,2,3,4,5,6]
+for x in mylist :
+    print(x)
